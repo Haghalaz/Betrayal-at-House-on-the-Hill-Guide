7882a34d8f4df58f64a6cd7515e0c89ea455c8ae
Luiz Coelho <luiz.adalberto@selettra.com.br>
2023-11-14 17:03:51 -0300

    feat:  Add home icon

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { GrHomeRounded } from "react-icons/gr";
+
 const Header = () => {
   return (
-    <div className="bg-[#000C04] w-screen h-16">
-      <p></p>
+    <div className="flex h-16 w-screen items-center justify-between bg-[#000C04] px-6 py-4">
+      <div className="flex items-center gap-2">
+        <GrHomeRounded className="h-4 w-4" />
+        <p>Sobre</p>
+      </div>
     </div>
   );
 };
