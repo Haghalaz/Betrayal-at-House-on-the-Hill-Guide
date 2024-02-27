4b264f18056bce8d2719f3165b70f1b8f8c24942
Luiz Coelho <luiz.adalberto@selettra.com.br>
2024-02-27 00:36:08 -0300

    fix: Change the name of the base src

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/betrayal-at-house-on-the-hill-guide/',
+  base: '/Betrayal-at-House-on-the-Hill-Guide/',
 
   resolve: {
     alias: {
